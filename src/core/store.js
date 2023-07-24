65851785882831003100fcc1dd3c98dce9dd2804
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-07-24 17:31:15 +0200

    store.js added

---
+++ src/core/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import personalHomepageReducer from './features/personalHomepage/personalHomepageSlice';
+import themeReducer from '../'
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+    reducer: {
+        personalHomepage: personalHomepageReducer,
+        theme: themeReducer,
+    },
+    middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(saga);
+
+export default store;
