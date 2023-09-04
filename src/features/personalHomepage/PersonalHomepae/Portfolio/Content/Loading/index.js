6ab4b8f9d370cbe16ae333c6a602ea62a8449207
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-09-04 13:00:19 +0200

    file location changed

--- src/features/personalHomepage/PersonalHomepae/Portfolio/Content/Loading/index.js
+++
@@ -1,8 +0,0 @@
-import { StyledLoading, Wrapper } from "./styled";
-
-export const Loading = () => (
-    <Wrapper>
-        Please wait, projects are being loaded...
-        <StyledLoading />
-    </Wrapper>
-);
--- src/features/personalHomepage/PersonalHomepae/Portfolio/githubUsername.js
+++
@@ -1,1 +0,0 @@
-export const gitHubUsername = "X-rey11"
