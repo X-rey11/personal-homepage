e8f36e80417b07db5d4c3c620ea55b5c69962197
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-09-06 15:26:41 +0200

    SocialIcons added

---
+++ src/features/personalHomepage/PersonalHomepage/Footer/SocialIcons/socials.js
@@ -0,0 +1,10 @@
+import { styledIcons } from "./styled";
+import { ReactComponent as GithubIcon } from "./icons/github.svg";
+
+export const socials = [
+    {
+        name: "GitHub",
+        url: "https://github.com/X-rey11",
+        Icon: styledIcons(GithubIcon),
+    },
+];
