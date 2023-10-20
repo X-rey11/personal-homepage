9c322fa187c9977c387cbfb381b1ee0cf56f6a16
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-10-20 14:32:03 +0200

    adding more socials

--- src/features/personalHomepage/PersonalHomepage/Footer/SocialIcons/socials.js
+++ src/features/personalHomepage/PersonalHomepage/Footer/SocialIcons/socials.js
@@ -1,10 +1,22 @@
 import { styledIcons } from "./styled";
 import { ReactComponent as GithubIcon } from "./icons/github.svg";
+import { ReactComponent as FacebookIcon } from "./icons/facebook.svg";
+import { ReactComponent as InstagramIcon } from "./icons/instagram.svg";
 
 export const socials = [
     {
         name: "GitHub",
         url: "https://github.com/X-rey11",
         Icon: styledIcons(GithubIcon),
+    },
+    {
+        name: "Facebook",
+        url: "https://www.facebook.com/profile.php?id=100010898799523&locale=pl_PL",
+        Icon: styledIcons(FacebookIcon),
+    },
+    {
+        name: "Instagram",
+        url: "https://www.instagram.com/mateusz.brysio/",
+        Icon: styledIcons(InstagramIcon),
     },
 ];
